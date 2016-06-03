fb23ea842c6ce9cb640e439b9a5eaa999a585bca
Bryce <bsaito88@gmail.com>
2016-06-02 19:20:46 -1000

    Added row test and row function, but only accepts non-negative test broke.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,3 +1,16 @@
-function createGrid () {
-  return document.documentElement;
+function createGrid (rows, x, y) {
+  var grid = document.createElement('div');
+    grid.className = 'grid';
+
+  var colElem = document.createElement('div');
+  for (var i = 0; i < rows; i++) {
+    if (rows >= 0) {
+      var rowElem = document.createElement('div');
+      rowElem.className = 'row';
+      grid.appendChild(rowElem);
+    } else {
+      throw new Error('Kaboom');
+    }
+  }
+  return grid;
 }
--- js/PixelPainter.spec.js
+++ js/PixelPainter.spec.js
@@ -1,14 +1,31 @@
 var expect = chai.expect;
 var should = chai.should();
+
+  beforeEach(function () {
+
+  });
+
   describe('createGrid()', function () {
 
-      it('should have a function called createGrid', function () {
-        expect(createGrid).to.exist;
-        expect(createGrid).to.be.a('function');
-      });
+    it('should have a function called createGrid', function () {
+      expect(createGrid).to.exist;
+      expect(createGrid).to.be.a('function');
+    });
 
-      it('should return an HTMLElement', function () {
-        expect(createGrid()).to.be.instanceof(HTMLElement);
-      });
+    it('should return an HTMLElement', function () {
+      expect(createGrid()).to.be.instanceof(HTMLElement);
+    });
+  });
 
+  describe('rows', function () {
+    it('should only accept a non-negative number', function () {
+      expect(createGrid.bind(null)).to.throw(Error);
+      expect(createGrid.bind(null, {})).to.throw(Error);
+      expect(createGrid.bind(null, -1)).to.throw(Error);
+      expect(createGrid.bind(null, 1)).to.not.throw(Error);
+    });
+    it('should return the same number of rows as provided', function() {
+      expect(createGrid(1).children).to.have.length(1);
+      expect(createGrid(10).children).to.have.length(10);
+    });
   });
